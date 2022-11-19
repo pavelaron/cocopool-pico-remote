1cbb46e7ba2f56a6081e28d13facd9284607e3c0
Aron Pavel <ext.aron.pavel@sats.no>
2022-11-19 23:28:41 +0100

    Zeroconf implementation

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import network
-import json
 import gc
+import pickle
 import utime as time
 from machine import Pin, Timer
 from microdot import Microdot, send_file
 
-config_file = open('config.json')
-config = json.loads(config_file.read())
-
-ssid = config['wlan']['ssid']
-password = config['wlan']['password']
+cache_filename = 'cache.dat'
+wlan_connection = network.STA_IF
 
 buttons = {
     'up': Pin(18, Pin.OUT),
@@ -17,29 +14,63 @@ buttons = {
     'down': Pin(20, Pin.OUT)
 }
 
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.config(pm = 0xa11140)
-wlan.connect(ssid, password)
-
-for i in range(10):
-    if wlan.status() < 0 or wlan.status() >= 3:
-        break
-    print('waiting for connection...')
-    time.sleep(1)
-
-if wlan.status() != 3:
-    raise RuntimeError('network connection failed')
-else:
-    print('connected')
-    status = wlan.ifconfig()
-    print('ip = ' + status[0])
-
 def button_press(button_key):
     button = buttons[button_key]
     button.value(1)
     timer = Timer(-1)
     timer.init(period=1000, mode=Timer.ONE_SHOT, callback=lambda t:button.value(0))
+
+def connect_sta(ssid, password):
+    wlan = network.WLAN(wlan_connection)
+    wlan.active(True)
+    wlan.config(pm=0xa11140)
+    wlan.connect(ssid, password)
+    
+    for i in range(10):
+        if wlan.status() < 0 or wlan.status() >= 3:
+            break
+        print('waiting for connection...')
+        time.sleep(1)
+    
+    if wlan.status() != 3:
+        init_ap()
+    else:
+        print('connected')
+        status = wlan.ifconfig()
+        print('ip = ' + status[0])
+        
+def init_ap():
+    global wlan_connection
+    wlan_connection = network.AP_IF
+    
+    ap = network.WLAN(wlan_connection)
+    ap.config(essid='Cocopool', password='123456789')
+    ap.active(True)
+    
+    print('Access point active')
+    print(ap.ifconfig())
+
+def restart():
+    gc.collect()
+    app.shutdown()
+    time.sleep(5)
+    start_server()
+
+def start_server():
+    try:
+        app.run(host='0.0.0.0', port=80)
+    except:
+        restart()
+
+try:
+    with open(cache_filename, 'rb') as cache:
+        data = pickle.load(cache)
+    
+    ssid = data['ssid']
+    password = data['password']
+    connect_sta(ssid, password)
+except Exception as error:
+    init_ap()
 
 gc.enable()
 gc.threshold(gc.mem_free() // 4 + gc.mem_alloc())
@@ -48,7 +79,23 @@ app = Microdot()
 
 @app.route('/')
 def index(request):
+    if wlan_connection == network.AP_IF:
+        return send_file('setup.html')
+    
     return send_file('index.html')
+
+@app.route('/save-ssid', methods=['POST'])
+def setup(request):
+    body = request.json
+    keys = body.keys()
+    
+    if 'ssid' not in keys or 'password' not in keys:
+        return 'bad request', 400
+    
+    with open(cache_filename, 'wb') as cache:
+        pickle.dump(body, cache)
+    
+    return '', 200
 
 @app.route('/static/<path:path>')
 def static(request, path):
@@ -64,17 +111,5 @@ def handle(request, direction):
 @app.errorhandler(RuntimeError)
 def runtime_error(request, exception):
     restart()
-
-def restart():
-    gc.collect()
-    app.shutdown()
-    time.sleep(5)
-    start_server()
-
-def start_server():
-    try:
-        app.run(host='0.0.0.0', port=80)
-    except:
-        restart()
 
 start_server()
