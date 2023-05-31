20ba19649d305c4a2b0b2b69c383b978f287f6ce
Aron Pavel <ext.aron.pavel@sats.no>
2023-05-31 14:44:06 +0200

    Added pipeline for compiling python sources

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
-const { task, src, dest, parallel } = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const htmlmin = require('gulp-htmlmin');
+const { obj } = require('through2');
+const { ensureDir } = require('fs-extra');
+const { join } = require('path');
+const { spawn } = require('child_process');
+const {
+  task,
+  src,
+  dest,
+  parallel,
+} = require('gulp');
 
 task('minify-css', () => (
   src('src/static/*.css')
@@ -21,20 +30,41 @@ task('minify-html', () => (
     .pipe(dest('dist'))
 ));
 
+task('mpy-cross', () => (
+  src('src/**/*.py')
+    .pipe(obj((chunk, _enc, cb) => {
+      const filepath = chunk.path.split('src/').pop();
+      const output = filepath.substring(0, filepath.lastIndexOf('.'));
+      const outputPath = `dist/${output}.mpy`;
+
+      ensureDir(join(__dirname, 'dist/lib'))
+        .then(() => {
+          spawn('mpy-cross', [`src/${filepath}`, '-o', outputPath])
+            .on('error', cb)
+            .on('exit', (code) => {
+              console.log('Out:', code, 'Created', outputPath);
+              cb(null, chunk);
+            });
+        })
+        .catch(cb);
+    }))
+));
+
 task('copy-lib', () => (
-  src('src/lib/**/*')
+  src(['src/lib/**/!(*.py)'])
     .pipe(dest('dist/lib'))
 ));
 
-task('copy-python', () => (
-  src('src/*.py')
-    .pipe(dest('dist'))
+task('copy-static', () => (
+  src(['src/static/**/!(*.css|*.js)'])
+    .pipe(dest('dist/static'))
 ));
 
 task('default', parallel(
+  'copy-lib',
+  'copy-static',
+  'mpy-cross',
   'minify-css',
   'minify-js',
   'minify-html',
-  'copy-lib',
-  'copy-python',
 ));
