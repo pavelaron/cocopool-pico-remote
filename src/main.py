28be94244c63c4b16421d7f23cfe8c11d6bfdd6b
Aron Pavel <ext.aron.pavel@sats.no>
2024-05-29 09:16:39 +0200

    Added handler for jeyboard interrupt

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ from logger import Logger
 if __name__ == '__main__':
     try:
         Cocopool()
+    except KeyboardInterrupt:
+        pass
     except Exception as error:
         Logger(error)
         machine.reset()
